fde9905c904dc338e0dc93d1950401b6c88e9ca6
mh-cbon <mh-cbon@users.noreply.github.com>
2016-06-15 17:01:35 +0200

    go fmt

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
-  "os"
-  "os/exec"
-  "strings"
-  "fmt"
+	"fmt"
+	"os"
+	"os/exec"
+	"strings"
 
 	"github.com/mh-cbon/build-them-all/utils"
 	"github.com/urfave/cli"
@@ -12,7 +12,7 @@ import (
 
 var VERSION = "0.0.0"
 
-func main () {
+func main() {
 	app := cli.NewApp()
 	app.Name = "build-them-all"
 	app.Version = VERSION
@@ -68,18 +68,18 @@ func main () {
 					Value: "",
 					Usage: "Working directory",
 				},
-        cli.BoolFlag{
-          Name:  "i",
-          Usage: "Installs the packages that are dependencies of the target",
-        },
-        cli.BoolFlag{
-          Name:  "a",
-          Usage: "Force rebuilding of packages that are already up-to-date",
-        },
-        cli.BoolFlag{
-          Name:  "n",
-          Usage: "Print the commands but do not run them",
-        },
+				cli.BoolFlag{
+					Name:  "i",
+					Usage: "Installs the packages that are dependencies of the target",
+				},
+				cli.BoolFlag{
+					Name:  "a",
+					Usage: "Force rebuilding of packages that are already up-to-date",
+				},
+				cli.BoolFlag{
+					Name:  "n",
+					Usage: "Print the commands but do not run them",
+				},
 				cli.StringFlag{
 					Name:  "p",
 					Value: "",
@@ -97,14 +97,14 @@ func main () {
 					Name:  "work",
 					Usage: "Print the name of the temporary work directory and do not delete it when exiting.",
 				},
-        cli.BoolFlag{
-          Name:  "race",
-          Usage: "Enable data race detection",
-        },
-        cli.BoolFlag{
-          Name:  "msan",
-          Usage: "Enable interoperation with memory sanitizer",
-        },
+				cli.BoolFlag{
+					Name:  "race",
+					Usage: "Enable data race detection",
+				},
+				cli.BoolFlag{
+					Name:  "msan",
+					Usage: "Enable interoperation with memory sanitizer",
+				},
 				cli.StringFlag{
 					Name:  "asmflags",
 					Value: "",
@@ -167,120 +167,120 @@ func main () {
 	app.Run(os.Args)
 }
 
-func clean (c *cli.Context) error {
-  errs := utils.CleanDirectories(c.StringSlice("dir"))
-  for _, err := range errs {
-    fmt.Println(err)
-  }
-  if len(errs)>0 {
-    return cli.NewExitError("Some files were not properly deleted", 1)
-  }
-  fmt.Println("Cleaned directories")
-  for _, d := range c.StringSlice("dir") {
-    fmt.Println(string(d))
-  }
-  return nil
+func clean(c *cli.Context) error {
+	errs := utils.CleanDirectories(c.StringSlice("dir"))
+	for _, err := range errs {
+		fmt.Println(err)
+	}
+	if len(errs) > 0 {
+		return cli.NewExitError("Some files were not properly deleted", 1)
+	}
+	fmt.Println("Cleaned directories")
+	for _, d := range c.StringSlice("dir") {
+		fmt.Println(string(d))
+	}
+	return nil
 }
 
-func build (c *cli.Context) error {
-  wantOs          := c.String("os")
-  wantArch        := c.String("arch")
-  output          := c.String("output")
-  gobin           := c.String("gobin")
-  wd              := c.String("wd")
-  dirs            := c.StringSlice("dir")
+func build(c *cli.Context) error {
+	wantOs := c.String("os")
+	wantArch := c.String("arch")
+	output := c.String("output")
+	gobin := c.String("gobin")
+	wd := c.String("wd")
+	dirs := c.StringSlice("dir")
 
-  packages := make([]string, 0)
-  for _, arg := range c.Args() {
-    packages = append(packages, arg)
-  }
-  if len(packages)==0 {
-    return cli.NewExitError("Packages list is required", 1)
-  }
+	packages := make([]string, 0)
+	for _, arg := range c.Args() {
+		packages = append(packages, arg)
+	}
+	if len(packages) == 0 {
+		return cli.NewExitError("Packages list is required", 1)
+	}
 
-  gobin, err := exec.LookPath(gobin)
-  if err != nil {
-    return cli.NewExitError("Path to go bin '"+gobin+"' was not found", 1)
-  }
+	gobin, err := exec.LookPath(gobin)
+	if err != nil {
+		return cli.NewExitError("Path to go bin '"+gobin+"' was not found", 1)
+	}
 
-  if len(wd)==0 {
-    wd, err = os.Getwd()
-    if err != nil {
-      return cli.NewExitError("Working directory could not be determined", 1)
-    }
-  }
+	if len(wd) == 0 {
+		wd, err = os.Getwd()
+		if err != nil {
+			return cli.NewExitError("Working directory could not be determined", 1)
+		}
+	}
 
-  errs :=utils.CleanDirectories(dirs)
-  for _, err := range errs {
-    fmt.Println(err)
-  }
-  if len(errs)>0 {
-    return cli.NewExitError("Some files were not properly deleted", 1)
-  }
+	errs := utils.CleanDirectories(dirs)
+	for _, err := range errs {
+		fmt.Println(err)
+	}
+	if len(errs) > 0 {
+		return cli.NewExitError("Some files were not properly deleted", 1)
+	}
 
-  fmt.Println("wd="+wd)
-  gotErr := false
-  selectedOsArch := utils.SelectOsArch(wantOs, wantArch)
-  for _, input := range packages {
-    pkg := utils.DetermineOutputName(input)
-    if len(pkg)==0 {
-      fmt.Println("Could not determine output name for '"+input+"'")
-      gotErr = true
-      break
-    }
-    for cOs, archs := range selectedOsArch {
-      for _, cArch := range archs {
+	fmt.Println("wd=" + wd)
+	gotErr := false
+	selectedOsArch := utils.SelectOsArch(wantOs, wantArch)
+	for _, input := range packages {
+		pkg := utils.DetermineOutputName(input)
+		if len(pkg) == 0 {
+			fmt.Println("Could not determine output name for '" + input + "'")
+			gotErr = true
+			break
+		}
+		for cOs, archs := range selectedOsArch {
+			for _, cArch := range archs {
 
-        bCmd := utils.BuildCommand{
-          Install:        c.Bool("i"),
-          RebuildAll:     c.Bool("a"),
-          N:              c.Bool("n"),
-          P:              c.String("p"),
-          Race:           c.Bool("race"),
-          Msan:           c.Bool("msan"),
-          V:              c.Bool("v"),
-          Work:           c.Bool("work"),
-          X:              c.Bool("x"),
-          Asmflags:       c.String("asmflags"),
-          Buildmode:      c.String("buildmode"),
-          Compiler:       c.String("compiler"),
-          Gccgoflags:     c.String("gccgoflags"),
-          Gcflags:        c.String("gcflags"),
-          InstallSuffix:  c.String("installsuffix"),
-          Ldflags:        c.String("ldflags"),
-          Linkshared:     c.String("linkshared"),
-          Pkgdir:         c.String("pkgdir"),
-          Tags:           c.String("tTags"),
-          Toolexec:       c.String("toolexec"),
-          Output:         utils.GenerateOutputName(output, pkg, cOs, cArch),
-          Wd:             wd,
-          Gobin:          gobin,
-        }
+				bCmd := utils.BuildCommand{
+					Install:       c.Bool("i"),
+					RebuildAll:    c.Bool("a"),
+					N:             c.Bool("n"),
+					P:             c.String("p"),
+					Race:          c.Bool("race"),
+					Msan:          c.Bool("msan"),
+					V:             c.Bool("v"),
+					Work:          c.Bool("work"),
+					X:             c.Bool("x"),
+					Asmflags:      c.String("asmflags"),
+					Buildmode:     c.String("buildmode"),
+					Compiler:      c.String("compiler"),
+					Gccgoflags:    c.String("gccgoflags"),
+					Gcflags:       c.String("gcflags"),
+					InstallSuffix: c.String("installsuffix"),
+					Ldflags:       c.String("ldflags"),
+					Linkshared:    c.String("linkshared"),
+					Pkgdir:        c.String("pkgdir"),
+					Tags:          c.String("tTags"),
+					Toolexec:      c.String("toolexec"),
+					Output:        utils.GenerateOutputName(output, pkg, cOs, cArch),
+					Wd:            wd,
+					Gobin:         gobin,
+				}
 
-        // build [-o output] [-i] [build flags] [packages]
-        oCmd := utils.GenerateBuildCommand(cOs, cArch, bCmd)
-        fmt.Println("> GOOS=" + cOs +" " + "GOARCH=" + cArch + " " +strings.Join(oCmd.Args, " "))
+				// build [-o output] [-i] [build flags] [packages]
+				oCmd := utils.GenerateBuildCommand(cOs, cArch, bCmd)
+				fmt.Println("> GOOS=" + cOs + " " + "GOARCH=" + cArch + " " + strings.Join(oCmd.Args, " "))
 
-        out, err := oCmd.CombinedOutput()
-        sOut := string(out)
-        if len(sOut)>0 {
-          fmt.Println(sOut)
-        }
-        if err==nil {
-          fmt.Println("Success!")
-        } else {
-          fmt.Println("Failure!")
-          fmt.Println(err)
-          gotErr = true
-        }
-        fmt.Println("")
-      }
-    }
-  }
+				out, err := oCmd.CombinedOutput()
+				sOut := string(out)
+				if len(sOut) > 0 {
+					fmt.Println(sOut)
+				}
+				if err == nil {
+					fmt.Println("Success!")
+				} else {
+					fmt.Println("Failure!")
+					fmt.Println(err)
+					gotErr = true
+				}
+				fmt.Println("")
+			}
+		}
+	}
 
-  if gotErr {
-    return cli.NewExitError("There were errors during the build", 1)
-  }
+	if gotErr {
+		return cli.NewExitError("There were errors during the build", 1)
+	}
 
-  return nil
+	return nil
 }
